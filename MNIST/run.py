ffb154700d95ccd439ee2b6b46bc6886283726ca
alexandreday <alexandre.day1@gmail.com>
2018-05-14 13:24:35 -0400

    debugging

--- MNIST/run.py
+++ MNIST/run.py
@@ -16,7 +16,7 @@ from sklearn.decomposition import PCA
 def main():
 
     X, y = dataset.load_mnist()
-    Xpca = PCA(n_components=100).fit_transform(X)
+    Xpca = PCA(n_components=40).fit_transform(X)
 
     model = CLUSTER(
         nh_size=40, n_cluster_init=20, n_iteration_tsne = 1000,
@@ -25,6 +25,11 @@ def main():
         run_tSNE='auto',
         root='/Users/alexandreday/GitProject/VAC/MNIST/info/'
     )
+
+    xtsne = pickle.load(open(model.file_name['tsne'],'rb'))
+    plotting.cluster_w_label(xtsne, y.flatten(), psize=5)
+    exit()
+
 
     np.random.seed(0)
     model.fit(Xpca)
